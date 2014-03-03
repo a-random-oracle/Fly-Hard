b143445d43fc5fe045f3ea953d59bdbad2dbab8c
a-random-oracle <ra728@york.ac.uk>
2014-03-03 21:00:14 +0000

    removed Score class

--- BTC/src/tst/AircraftTest.java
+++ BTC/src/tst/AircraftTest.java
@@ -9,19 +9,16 @@ import scn.Demo;
 import cls.Aircraft;
 import cls.Waypoint;
 import cls.Vector;
-import cls.Score;
 
 @SuppressWarnings("deprecation")
 
 public class AircraftTest {	
 	Aircraft testAircraft;
-	Score testScore;
 	
 	@Before
 	public void setUp() {
 		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
 		testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
-		testScore = new Score();
 	}
 	
 	// Test get functions
