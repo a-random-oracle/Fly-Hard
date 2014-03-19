6265f72f746bf93b8a931c1834e8e2046c68287e
R Kirby <rk755@york.ac.uk>
2014-03-19 18:07:33 +0000

    ThreadReceive class added back in under net package

---
+++ BTC/src/net/ThreadReceive.java
@@ -0,0 +1,33 @@
+package net;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import scn.MultiPlayerGame;
+
+public class ThreadReceive extends Thread {
+	
+	ObjectInputStream inStream;
+	MultiPlayerGame multiPlayerGame;
+		
+	public ThreadReceive(MultiPlayerGame multiPlayerGame, ObjectInputStream inStream) {
+		this.inStream = inStream;
+		this.multiPlayerGame = multiPlayerGame;
+		start();
+		System.out.println(multiPlayerGame.getPlayers().get(0).getScore());
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				multiPlayerGame.getPlayers().get(0).setScore((
+						(MultiPlayerGame) inStream.readObject()).getPlayers().get(0).getScore());
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
