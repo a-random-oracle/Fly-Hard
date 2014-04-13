fda1d5767f961b569a124fa403b57bae9b9907d7
Richard Kirby <rk755@york.ac.uk>
2014-04-13 03:12:10 +0100

    Powerup class now contains an enum to store all the power-ups. Appropiate methods have been partially introduced to aid in the random generation and setting of power-ups.

--- BTC/src/cls/Powerup.java
+++ BTC/src/cls/Powerup.java
@@ -1,8 +1,10 @@
 package cls;
 
-import org.newdawn.slick.Image;
+import java.io.File;
+import java.util.Random;
 
 import lib.jog.graphics;
+import lib.jog.graphics.Image;
 
 public class Powerup {
 	
@@ -17,7 +19,30 @@ public class Powerup {
 
 	/** Image used for powerup */
 	private Image image;
-
+	
+	/** <p>The <code>PowerUp</code> enum contains all power-ups featured in the game.
+	 * </p>This enum gives access to a random power-up value; to be used in spawning
+	 * a random powerup during run time.</p>
+	 * The power-ups are as follows:</p>
+	 * <code>PowerUp.FOG - "description here"<p>
+	 * <code>PowerUp.SLOW_DOWN - "description here"<p>
+	 * <code>PowerUp.SPEED_UP - "description here"<p>
+	 * <code>PowerUp.TRANSFER - "description here"<p>
+	 *  */
+	protected enum PowerUp {
+		FOG,
+		SLOW_DOWN,
+		SPEED_UP,
+		TRANSFER;
+		
+		private static final PowerUp[] POWER_UP_NAMES = values();
+		private static final int SIZE = POWER_UP_NAMES.length;
+		private static final Random RANDOM = new Random();
+		
+		public static PowerUp randomPowerUp()  {
+		    return POWER_UP_NAMES[RANDOM.nextInt(SIZE) - 1];
+		  }
+	}
 	
 	/**
 	 * Constructor for powerup.
@@ -30,11 +55,11 @@ public class Powerup {
 	 * @param duration
 	 * 			the length the powerup is active for
 	 */
-	public Powerup(double x, double y, String name, Image image, int duration){
+	public Powerup(double x, double y, String name, int duration){
 		this.powerupLocation = new Vector(x, y, 0);
 		this.name = name;
 		this.duration = duration;
-		this.image = image;
+		this.setPowerUpImage();
 	}
 	
 	//draw the powerup icon, depending on waypoint
@@ -55,6 +80,33 @@ public class Powerup {
 	
 	public void chooseRandomWaypoint() {
 		
+	}
+	
+	private void setUpPowerUp() {
+		
+	}
+	
+	/**
+	 * The <code>setPowerUpImage</code> method initialises the power-up's image
+	 * to the appropriate image based on the value randomly chosen by the <code>PowerUp</code> enum.
+	 */
+	private void setPowerUpImage() {
+		PowerUp powerUp = PowerUp.randomPowerUp();
+		
+		switch (powerUp) {
+		case FOG: 
+			image = graphics.newImage("gfx.pUp" + File.separator + "SET TO FILE NAME HERE");
+			break;
+		case SLOW_DOWN:
+			image = graphics.newImage("gfx.pUp" + File.separator + "SET TO FILE NAME HERE");
+			break;
+		case SPEED_UP:
+			image = graphics.newImage("gfx.pUp" + File.separator + "SET TO FILE NAME HERE");
+			break;
+		case TRANSFER:
+			image = graphics.newImage("gfx.pUp" + File.separator + "SET TO FILE NAME HERE");
+			break;
+		}
 	}
 	
 	
