f1f106038325c667f220da13b53453d24bbfecec
random_oracle <ra728@york.ac.uk>
2014-04-30 12:30:49 +0100

    updated version number

--- BTC/src/btc/Main.java
+++ BTC/src/btc/Main.java
@@ -38,7 +38,7 @@ public class Main implements input.EventHandler {
 	
 	
 	/** The game's version number */
-	public static final String VERSION = "Fly-Hard-0.1";
+	public static final String VERSION = "Fly-Hard-0.2";
 
 	/** The title to display in the game window */
 	private final String TITLE = "Bear Traffic Controller: GOA Edition";
