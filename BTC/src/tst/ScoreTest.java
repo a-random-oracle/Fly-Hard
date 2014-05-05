f666d48b4501e54be56a0d417cc0774328bfe487
Emily Hall <eh819@york.ac.uk>
2014-05-05 23:11:44 +0100

    Testing

--- BTC/src/tst/ScoreTest.java
+++ BTC/src/tst/ScoreTest.java
@@ -9,6 +9,7 @@ import scn.SinglePlayerGame;
 import scn.Game.DifficultySetting;
 import cls.Aircraft;
 import cls.Airport;
+import cls.Player;
 import cls.Waypoint;
 
 public class ScoreTest {
@@ -22,6 +23,12 @@ public class ScoreTest {
 	Airport testAirport;
 	
 	Airport testAirport2;
+	
+	Airport airports[];
+	
+	Player testPlayer;
+	
+	Aircraft[] aircrafts;
 	
 	/**
 	 * Sets up the tests.
@@ -38,22 +45,35 @@ public class ScoreTest {
 				new Waypoint(75, 25, false, false),
 				new Waypoint(50,50, false, false)
 				};
+		airports = new Airport[] {
+				new Airport("Babbage International", (1d/7d), (1d/2d)),
+				new Airport("Eboracum Airport", (6d/7d), (1d/2d))
+		};
+		
+		testPlayer = new Player(0, airports, waypointList);
+		
 		
 		testAirport = new Airport("Babbage International", (1d/7d), (1d/2d));
 		testAirport2 = new Airport("Babbage International", (1d/7d), (1d/2d));
 		
 		testAircraft = new Aircraft("TSTAircraft", "TestAir", "Berlin", "Dublin",
 				new Waypoint(100, 100, true, false), new Waypoint(0, 0, true, false),
-				10.0, waypointList, DifficultySetting.MEDIUM, testAirport, testAirport2);	
+				10.0, waypointList, DifficultySetting.MEDIUM, null, null);	
+		aircrafts = new Aircraft[1];
+		
+		
+		testPlayer.getAircraft().add(testAircraft);
 	}
 	
-	/**
-	 * Tests that score is decremented when an aircraft's flight path is altered.
-	 */
 	@Test
-	public void testScoreDecrementAlterPath() {
-		testAircraft.alterPath(1, new Waypoint(25, 75, false, false));
-		assertTrue("Score not decremented successfully", testAircraft.getScore()==99);
+	public void scoreStartsAtZero() {
+		assertTrue("Score does not start at correct value", testPlayer.getScore() == 0);
+	}
+	
+	@Test
+	public void testAddScoreToPlayer() {
+		testPlayer.increaseScore(testAircraft.getScore());
+		assertTrue("Score not added to player successfully", testPlayer.getScore() == testAircraft.getScore());
 	}
 	
 }
